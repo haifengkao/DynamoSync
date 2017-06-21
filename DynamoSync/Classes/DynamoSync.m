d3eb6546d127f3ec0611f25dbb6b006ef4032ad5
Hai Feng Kao <haifeng@cocoaspice.in>
2017-06-21 19:12:19 +0800

    initial commit

---
+++ DynamoSync/Classes/DynamoSync.m
@@ -0,0 +1,21 @@
+//
+//  DynamoSync.m
+//  DynamoSync
+//
+//  Created by Hai Feng Kao on 2017/06/21.
+//
+//
+
+#import "DynamoSync.h"
+
+@implementation DynamoSync
+
+- (instancetype)init
+{
+    if (self = [super init]) {
+        // TODO
+    }
+    return self;
+}
+
+@end
--- Example/Pods/Target Support Files/DynamoSync/DynamoSync-umbrella.h
+++ Example/Pods/Target Support Files/DynamoSync/DynamoSync-umbrella.h
@@ -10,6 +10,7 @@
 #endif
 #endif
 
+#import "DynamoSync.h"
 
 FOUNDATION_EXPORT double DynamoSyncVersionNumber;
 FOUNDATION_EXPORT const unsigned char DynamoSyncVersionString[];
